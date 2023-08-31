bd46a0032930e72dda940e39f5903b7b952ac246
EdwardJhosep <1454668@senati.pe>
2023-08-31 17:49:02 -0500

    agregar numero para identificar a cada boton

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -50,12 +50,12 @@ namespace WinFormsApp1
 
         private void button10_Click(object sender, EventArgs e)//numero5
         {
-           
+            textBox1.Text += "5";
         }
 
         private void button14_Click(object sender, EventArgs e)//numero2
         {
-           
+            textBox1.Text += "2";
         }
 
         private void button12_Click(object sender, EventArgs e)//←
@@ -70,37 +70,37 @@ namespace WinFormsApp1
 
         private void button5_Click(object sender, EventArgs e)//numero7
         {
-
+            textBox1.Text += "7";
         }
 
         private void button6_Click(object sender, EventArgs e)//numero8
         {
-
+            textBox1.Text += "8";
         }
 
         private void button11_Click(object sender, EventArgs e)//numero6
         {
-    
+            textBox1.Text += "6";
         }
 
         private void button7_Click(object sender, EventArgs e)//numero9
         {
-
+            textBox1.Text += "9";
         }
 
         private void button9_Click(object sender, EventArgs e)//numero4
         {
-  
+            textBox1.Text += "4";
         }
 
         private void button13_Click(object sender, EventArgs e)//numero1
         {
-      
+            textBox1.Text += "1";
         }
 
         private void button15_Click(object sender, EventArgs e)//numero3
         {
-
+            textBox1.Text += "3";
         }
 
         private void button16_Click(object sender, EventArgs e)//IGUAL =
@@ -110,7 +110,7 @@ namespace WinFormsApp1
 
         private void button17_Click(object sender, EventArgs e)//numero0
         {
-      
+            textBox1.Text += "0";
         }
 
         private void button18_Click(object sender, EventArgs e)// PUNTO .
