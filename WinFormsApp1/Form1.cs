ef01f52f568ce5cd483e8e361b2f174824f5ded4
EdwardJhosep <1454668@senati.pe>
2023-08-31 17:57:15 -0500

    crear funcion realizaroperacion para todas los operaciones

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -131,6 +131,30 @@ namespace WinFormsApp1
 
         }
 
+        private double RealizarOperacion(double num1, double num2, char operador)
+        {
+            double resultado = 0;
 
+            switch (operador)
+            {
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+                case '-':
+                    resultado = num1 - num2;
+                    break;
+                case '*':
+                    resultado = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                        resultado = num1 / num2;
+                    else
+                        MessageBox.Show("No se puede dividir por cero.");
+                    break;
+            }
+
+            return resultado;
+        }
     }
-    }
+}
